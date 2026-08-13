cb8d8713867499ff8e78b999c9befc01002ddc02
Yessica Jaramillo <yuliethjaramillo1916@gmail.com>
2026-08-13 13:42:12 -0500

    Sincronizar compilado final de Vite en la carpeta dist de la raiz e incluir imagenes en dist/imagen

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
   server: {
     port: 3000,
     open: true
